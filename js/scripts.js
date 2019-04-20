83e6cf75d70cd4f9146780a6fd6b27d51430f8aa
Stewart ilondanga <stilondanga@gmail.com>
2019-04-20 13:12:48 +0300

    add js/scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -33,113 +33,113 @@ function changeDate(val) {
   dd = today.getDate();
   yyyy = today.getFullYear();
   mm = months[today.getMonth()];
-  /*    drawDate();
+  drawDate();
+}
+/*  function checkIfToday(val){
+    var checkArr = (today.getDate() + val).toString().split(" ");
+    var todayArr = today.getDate().toString().split(" ");
+    var todayStr = chopDate(todayArr);
+    var checkStr = chopDate(checkArr);
+    if(checkStr != todayStr) {
+      $(".today").show();
     }
-    function checkIfToday(val){
-      var checkArr = (today.getDate() + val).toString().split(" ");
-      var todayArr = today.getDate().toString().split(" ");
-      var todayStr = chopDate(todayArr);
-      var checkStr = chopDate(checkArr);
-      if(checkStr != todayStr) {
-        $(".today").show();
-      }
+  }
+  function chopDate(day){
+    var arr = [];
+    for(var i = 0; i < 4; i++){
+      arr.push(day[i]);
     }
-    function chopDate(day){
-      var arr = [];
-      for(var i = 0; i < 4; i++){
-        arr.push(day[i]);
-      }
-      var str = arr.join("");
-      return str;
+    var str = arr.join("");
+    return str;
+  }
+  function drawDate(){
+    getLocalStorage();
+    var str = dd.toString();
+    str = parseInt(str[str.length -1]);
+    var supArr = ["th", "st", "nd", "rd"]
+    if(str != 1 && str != 2 && str != 3){
+      str = 0;
     }
-    function drawDate(){
-      getLocalStorage();
-      var str = dd.toString();
-      str = parseInt(str[str.length -1]);
-      var supArr = ["th", "st", "nd", "rd"]
-      if(str != 1 && str != 2 && str != 3){
-        str = 0;
-      }
-      var sup = supArr[str];
+    var sup = supArr[str];
 
-      $("#month").text(mm);
-      $("#day").text(dd);
-      $("#super").text(sup);
-      $("#year").text(yyyy);
-    }
-    var template = ``;
-    for(var i = 1; i <= 24; i++){
-      var amPm = (i < 12) ? "AM":"PM";
-      var hour = (i < 13) ? i : i - 12;
-      template += `<div class="hour" id="${i}"><sub class="left">${hour}:00 ${amPm}</sub><div class="hour-input" id="hour${i}"></div></div>`;
-    }
-    $("#time").html(template)
+    $("#month").text(mm);
+    $("#day").text(dd);
+    $("#super").text(sup);
+    $("#year").text(yyyy);
+  }
+  var template = ``;
+  for(var i = 1; i <= 24; i++){
+    var amPm = (i < 12) ? "AM":"PM";
+    var hour = (i < 13) ? i : i - 12;
+    template += `<div class="hour" id="${i}"><sub class="left">${hour}:00 ${amPm}</sub><div class="hour-input" id="hour${i}"></div></div>`;
+  }
+  $("#time").html(template)
 
-    $(".hour").click(function(e){
-      textWrite(e);
+  $(".hour").click(function(e){
+    textWrite(e);
+  });
+  $(".hour-input").click(function(e){
+    var val = e.target.id[e.target.id.length -1];
+    var result = {target: {id: val}}
+    textWrite(result);
+  });
+  function textWrite(val){
+    var id = val.target.id;
+    var text = $(`#hour${id}`).text();
+    $(`#hour${id}`).html(`<input id="input${id}" value="${text}">`)
+    $(`#input${id}`).focus();
+    currentInput = id;
+    $(`#hour${id}`).focusout(function(){
+      var val = $(`#input${id}`).val();
+      $(`#hour${id}`).text(val)
     });
-    $(".hour-input").click(function(e){
-      var val = e.target.id[e.target.id.length -1];
-      var result = {target: {id: val}}
-      textWrite(result);
-    });
-    function textWrite(val){
-      var id = val.target.id;
-      var text = $(`#hour${id}`).text();
-      $(`#hour${id}`).html(`<input id="input${id}" value="${text}">`)
-      $(`#input${id}`).focus();
-      currentInput = id;
-      $(`#hour${id}`).focusout(function(){
-        var val = $(`#input${id}`).val();
-        $(`#hour${id}`).text(val)
-      });
-    }
-    var currentInput = "";
+  }
+  var currentInput = "";
 
-      $(document).on('keydown', 'input', function(e) {
-        var keyCode = e.keyCode || e.which;
-        if (keyCode == 9 && currentInput != 24) {
-          e.preventDefault();
-          var num = parseInt(currentInput);
-          num++
-          currentInput = num;
-            var result = {target: {id: currentInput}}
-            textWrite(result);
-        }
-      });
-    $(".save").click(function(){
-      var arr = getValues();
-      var day = chopDate(currentDay);
-      localStorage.setItem(day,JSON.stringify(arr));
+    $(document).on('keydown', 'input', function(e) {
+      var keyCode = e.keyCode || e.which;
+      if (keyCode == 9 && currentInput != 24) {
+        e.preventDefault();
+        var num = parseInt(currentInput);
+        num++
+        currentInput = num;
+          var result = {target: {id: currentInput}}
+          textWrite(result);
+      }
     });
-    function getValues(){
-      var hourArr = [];
+  $(".save").click(function(){
+    var arr = getValues();
+    var day = chopDate(currentDay);
+    localStorage.setItem(day,JSON.stringify(arr));
+  });
+  function getValues(){
+    var hourArr = [];
+    for(var i = 1; i < 25; i++){
+      hourArr.push($("#hour" + i).text());
+    }
+    console.log("Get values",hourArr);
+    return hourArr;
+  }
+  function getLocalStorage(){
+    var hourArr = [];
+    hourArr = JSON.parse(localStorage.getItem(chopDate(currentDay)));
+    console.log(hourArr);
+    if(hourArr != null){
+      //console.log(chopDate(currentDay)+":",hourArr);
       for(var i = 1; i < 25; i++){
-        hourArr.push($("#hour" + i).text());
-      }
-      console.log("Get values",hourArr);
-      return hourArr;
-    }
-    function getLocalStorage(){
-      var hourArr = [];
-      hourArr = JSON.parse(localStorage.getItem(chopDate(currentDay)));
-      console.log(hourArr);
-      if(hourArr != null){
-        //console.log(chopDate(currentDay)+":",hourArr);
-        for(var i = 1; i < 25; i++){
-          if(hourArr[i] != null && hourArr[i] != undefined){
-            //console.log(hourArr[i]);
-            $("#hour" + i).text( hourArr[i]);
-          }
-          else{
-            $("#hour" + i).text("");
-          }
+        if(hourArr[i] != null && hourArr[i] != undefined){
+          //console.log(hourArr[i]);
+          $("#hour" + i).text( hourArr[i]);
         }
-      }
-      else {
-        //console.log(chopDate(currentDay)+":",hourArr);
-        for(var i = 1; i < 25; i++){
+        else{
           $("#hour" + i).text("");
         }
       }
     }
+    else {
+      //console.log(chopDate(currentDay)+":",hourArr);
+      for(var i = 1; i < 25; i++){
+        $("#hour" + i).text("");
+      }
+    }
+  }
